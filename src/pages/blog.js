b0a7f385d94c2f94e4deab8321353ed5939c71a6
Takaaki-Shimizu <tattaka_feelmysoul@yahoo.co.jp>
2022-05-31 08:06:04 +0900

    blogコンポーネントに必要なデータ加工

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,4 +1,6 @@
-const Blog = () => {
+import matter from "gray-matter";
+
+const Blog = (props) => {
   return (
     <div>
       <h1>ブログページ</h1>
@@ -7,3 +9,24 @@ const Blog = () => {
 };
 
 export default Blog;
+
+export async function getStaticProps() {
+  const blogs = ((context) => {
+    const keys = context.keys();
+    const values = keys.map(context);
+		const data = keys.map((key, index) => {
+			let slug = key.replace(/^.*[\\\/]/, ").slice(0, -3)
+			const value = value[index]
+			const document = matter(value.default)
+			return {
+				frontmatter: document.data,
+				slug: slug
+			}
+		})
+		return data
+  })(require.context("../data", true, /\.md$/));
+	console.log(blogs);
+  return {
+    props: {},
+  };
+}
