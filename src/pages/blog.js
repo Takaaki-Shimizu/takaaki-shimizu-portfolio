c97210d05edf9aad79470dddd47d4b5a8b7379b8
Takaaki-Shimizu <tattaka_feelmysoul@yahoo.co.jp>
2022-06-03 23:42:10 +0900

    ファイル名修正

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,9 +1,20 @@
+import Link from "next/link";
 import matter from "gray-matter";
 
-const Blog = (props) => {
+const Blog = ({ blogs }) => {
   return (
     <div>
       <h1>ブログページ</h1>
+      {blogs.map((blog, index) => {
+        console.log(blog);
+        <div key={index}>
+          <h3>{blog.frontmatter.title}</h3>
+          <p>{blog.frontmatter.date}</p>
+          <Link href={`/blog/${blog.slug}`}>
+            <a>Read More</a>
+          </Link>
+        </div>;
+      })}
     </div>
   );
 };
@@ -14,19 +25,18 @@ export async function getStaticProps() {
   const blogs = ((context) => {
     const keys = context.keys();
     const values = keys.map(context);
-		const data = keys.map((key, index) => {
-			let slug = key.replace(/^.*[\\\/]/, ").slice(0, -3)
-			const value = value[index]
-			const document = matter(value.default)
-			return {
-				frontmatter: document.data,
-				slug: slug
-			}
-		})
-		return data
+    const data = keys.map((key, index) => {
+      let slug = key.replace(/^.*[\\\/]/, "").slice(0, -3);
+      const value = values[index];
+      const document = matter(value.default);
+      return {
+        frontmatter: document.data,
+        slug: slug,
+      };
+    });
+    return data;
   })(require.context("../data", true, /\.md$/));
-	console.log(blogs);
   return {
-    props: {},
+    props: { blogs: JSON.parse(JSON.stringify(blogs)) },
   };
 }
