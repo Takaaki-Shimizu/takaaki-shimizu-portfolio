38ffec5f67bf80a221f69c4497e6fcf427ab85f1
Takaaki-Shimizu <tattaka_feelmysoul@yahoo.co.jp>
2022-06-06 23:07:57 +0900

    [slug].jsの作成

---
+++ src/pages/blog/[slug].js
@@ -0,0 +1,25 @@
+const SingleBlog = () => {
+  return (
+    <div>
+      <h1>記事ページ</h1>
+    </div>
+  );
+};
+
+export default SingleBlog;
+
+export async function getStaticPaths() {
+  const blogSlugs = ((context) => {
+    const keys = context.keys();
+    const data = keys.map((key, index) => {
+      let slug = key.replace(/^.*[\\\/]/, "").slice(0, -3);
+      return slug;
+    });
+    return data;
+  })(require.context("../data", true, /\.md$/));
+  const paths = blogSlugs.map((blogSlug) => `/blog/${blogSlug}`);
+  return {
+    paths: paths,
+    fallback: false,
+  };
+}
